a1e18be6a03fd24e348be73b74564eff68b48552
lilendian0x00 <arianth82@gmail.com>
2022-12-03 12:49:07 +0330

    added fiber routes and handler for them

--- controllers/todosController.go
+++ controllers/todosController.go
@@ -1,1 +1,55 @@
 package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/lilendian0x00/go-mvc/initializers"
+	"github.com/lilendian0x00/go-mvc/models"
+)
+
+type resp struct {
+	Status			string 		`json:"status"`
+	Msg				string		`json:"msg"`
+}
+
+func TodosIndex(c *fiber.Ctx) error {
+	result := initializers.DB.Find(&models.Todo{})
+	return c.Status(200).JSON(result)
+}
+
+func AddTodo(c *fiber.Ctx) error {
+	todo :=  new(models.Todo)
+	if err := c.BodyParser(&todo); err != nil {
+		c.Status(400).JSON(resp{
+			Status: "failure",
+			Msg:    "bad post body!",
+		})
+		return err
+	}
+
+	initializers.DB.Create(&todo)
+
+	return c.Status(200).JSON(resp{
+		Status: "success",
+		Msg:    "todo added successfully!",
+	})
+}
+
+func RemoveTodo(c *fiber.Ctx) error {
+	todo :=  new(models.Todo)
+	if err := c.BodyParser(&todo); err != nil {
+		c.Status(400).JSON(resp{
+			Status: "failure",
+			Msg:    "bad post body!",
+		})
+		return err
+	}
+	initializers.DB.Delete(&todo)
+
+	return c.Status(200).JSON(resp{
+		Status: "success",
+		Msg:    "todo deleted successfully!",
+	})
+}
+
+
+
--- routes.go
+++ routes.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/lilendian0x00/go-mvc/controllers"
+)
+
+func SetupRoutes(app *fiber.App) {
+	/* \/api\/ */
+	api := app.Group("/api")
+	v1 := api.Group("/v1", func(ctx *fiber.Ctx) error {
+		ctx.Accepts("application/json")
+		return ctx.Next()
+	})
+
+	v1.Get("/list", controllers.TodosIndex)
+
+	v1.Post("/add", controllers.AddTodo)
+
+	v1.Delete("/remove", controllers.RemoveTodo)
+}
