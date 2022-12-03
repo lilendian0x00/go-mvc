07ffa65a36e171941bf31e74b9fb7803ebf2c466
lilendian0x00 <arianth82@gmail.com>
2022-12-03 12:55:28 +0330

    transferred routes from routes.go to the main.go because of an unkown error

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/lilendian0x00/go-mvc/configs"
+	"github.com/lilendian0x00/go-mvc/controllers"
 	"github.com/lilendian0x00/go-mvc/initializers"
 	"github.com/lilendian0x00/go-mvc/utils"
 	"log"
@@ -21,12 +22,28 @@ func init() {
 	}
 
 	initializers.ConnectToDatabase(Configs.DBSource)
+
 	// migrate models
 	initializers.SyncDB()
 }
 
 func serveStatic(app *fiber.App) {
 	app.Static("/", "./views/build")
+}
+
+func setupRoutes(app *fiber.App) {
+	/* \/api\/ */
+	api := app.Group("/api")
+	v1 := api.Group("/v1", func(ctx *fiber.Ctx) error {
+		ctx.Accepts("application/json")
+		return ctx.Next()
+	})
+
+	v1.Get("/list", controllers.TodosIndex)
+
+	v1.Post("/add", controllers.AddTodo)
+
+	v1.Delete("/remove", controllers.RemoveTodo)
 }
 
 func main() {
@@ -37,7 +54,7 @@ func main() {
 	serveStatic(app)
 
 	// setup api routes
-	SetupRoutes(app)
+	setupRoutes(app)
 
 	log.Printf("[REST] listening at %v\n", Configs.ServerAddress)
 	err := app.Listen(Configs.ServerAddress)
